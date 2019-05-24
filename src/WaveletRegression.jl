24333f45bb4c0a7738272a23d2b7564a7c31d8a5
Samuel Urmy <urmy@shore.mbari.org>
2019-05-24 14:38:16 -0700

    Type to encapsulate wavelet transform (wavelet + nlevels)

--- src/WaveletRegression.jl
+++ src/WaveletRegression.jl
@@ -2,49 +2,73 @@ module WaveletRegression
 
 using Wavelets
 using Statistics
-export wavelm, WaveletRegressionModel, modwt_matrix, imodwt_matrix, predict
+export
+    wavelm,
+    AbstractWaveletTransform,
+    MODWT,
+    DWT,
+    DWPT,
+    WaveletRegressionModel,
+    modwt_matrix,
+    imodwt_matrix,
+    predict
 export wavelet, WT
 
-struct WaveletRegressionModel
-    X
-    Y
-    Xw
-    Yw
-    wt
-    nlevels
-    coefficients
-    Xintercept
-    Yintercept
+abstract type AbstractWaveletTransform end
+
+for transformtype in [:MODWT, :DWT, :DWPT]
+    @eval begin
+        struct $transformtype <: AbstractWaveletTransform
+            wavelet::DiscreteWavelet
+            nlevels::Integer
+        end
+        Wavelets.wavelet(trans::$(transformtype)) = trans.wavelet
+        nlevels(trans::$(transformtype)) = trans.nlevels
+    end
 end
+
+struct WaveletRegressionModel
+    X::AbstractArray
+    Y::AbstractArray
+    n::Integer
+    mx::Integer
+    my::Integer
+    Xw::AbstractArray
+    Yw::AbstractArray
+    trans::AbstractWaveletTransform
+    coefficients
+end
+Wavelets.wavelet(mod::WaveletRegressionModel) = wavelet(mod.trans)
+nlevels(mod::WaveletRegressionModel) = nlevels(mod.trans)
 
 function predict(mod::WaveletRegressionModel, newX)
     @assert size(newX) == size(mod.X)
-    newXw = modwt_matrix(newX, mod.wt, mod.nlevels)
-    n, my = size(mod.Y)
-    Yw = cat([reshape(newXw[:, j, :] * mod.coefficients[j], (n, 1, my))
-        for j in 1:mod.nlevels+1]..., dims=2)
-    Y = imodwt_matrix(Yw, mod.wt)
+    newXw = modwt_matrix(newX, mod.trans)
+    Yw = cat([reshape(newXw[:, j, :] * mod.coefficients[j], (mod.n, 1, mod.my))
+        for j in 1:nlevels(mod)+1]..., dims=2)
+    Y = imodwt_matrix(Yw, mod.trans)
     return Y
 end
 predict(mod::WaveletRegressionModel) = predict(mod, mod.X)
 
-function modwt_matrix(X, wt, nlevels)
-    return cat([modwt(X[:, i], wt, nlevels) for i in 1:size(X, 2)]..., dims=3)
+function modwt_matrix(X::AbstractVecOrMat, trans::MODWT)
+    return cat([modwt(X[:, i], trans.wavelet, trans.nlevels) for i in 1:size(X, 2)]..., dims=3)
 end
 
-function imodwt_matrix(Xw, wt)
+function imodwt_matrix(Xw::AbstractArray, trans::MODWT)
     n, nlevels, nvars = size(Xw)
-    hcat([imodwt(Xw[:, :, i], wt) for i in 1:nvars]...)
+    hcat([imodwt(Xw[:, :, i], trans.wavelet) for i in 1:nvars]...)
 end
 
-function wavelm(X::AbstractVecOrMat, Y::AbstractVecOrMat, wt, nlevels)
-    Xintercept = mean(X, dims=1)
-    Yintercept = mean(Y, dims=1)
-    Xw = modwt_matrix(X, wt, nlevels)
-    Yw = modwt_matrix(Y, wt, nlevels)
-    BB = [Xw[:, j, :] \ Yw[:, j, :] for j in 1:(nlevels+1)]
-    return WaveletRegressionModel(X, Y, Xw, Yw, wt, nlevels, BB, Xintercept, Yintercept)
-end
 
+function wavelm(X::AbstractVecOrMat, Y::AbstractVecOrMat, trans::MODWT)
+    Xw = modwt_matrix(X, trans)
+    Yw = modwt_matrix(Y, trans)
+    BB = [Xw[:, j, :] \ Yw[:, j, :] for j in 1:(trans.nlevels+1)]
+    n = size(X, 1)
+    mx = div(length(X), n)
+    my = div(length(Y), n)
+    return WaveletRegressionModel(X, Y, n, mx, my, Xw, Yw, trans, BB)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,7 @@ macro setupvars()
         my = 3
         wt = wavelet(WT.db2)
         nlevels = 4
+        trans = MODWT(wt, nlevels)
     end
     return esc(ex)
 end
@@ -18,8 +19,8 @@ end
 @testset "Utilities" begin
     @setupvars
     X = randn(n, mx)
-    Xw = modwt_matrix(X, wt, nlevels)
-    X1 = imodwt_matrix(Xw, wt)
+    Xw = modwt_matrix(X, trans)
+    X1 = imodwt_matrix(Xw, trans)
     @test size(Xw) == (n, nlevels+1, mx)
     @test size(X1) == size(X)
     @test all(X .≈ X1)
@@ -30,9 +31,11 @@ end
     @setupvars
     X = randn(n)
     Y = randn(n)
-    res1 = wavelm(X, Y, wt, nlevels)
+    res1 = wavelm(X, Y, trans)
     @test length(res1.coefficients) == nlevels + 1
     @test all(size(B) == (1, 1) for B  in res1.coefficients)
+    Y_pred = predict(res1)
+    Y_pred = predict(res1, randn(size(X)))
 end
 
 # matrix X and Y
@@ -40,33 +43,9 @@ end
     @setupvars
     X = randn(n, mx)
     Y = randn(n, my)
-    res1 = wavelm(X, Y, wt, nlevels)
+    res1 = wavelm(X, Y, trans)
     @test length(res1.coefficients) == nlevels + 1
     @test all(size(B) == (mx, my) for B  in res1.coefficients)
+    Y_pred = predict(res1)
+    Y_pred = predict(res1, randn(size(X)))
 end
-
-# @setupvars
-# coefficients = [randn(mx, my) for i in 1:nlevels+1]
-# Xw = modwt_matrix(X, wt, nlevels)
-# @test all(imodwt_matrix(Xw, wt) .≈ X)
-# @test all(modwt_matrix(X, wt, nlevels) .≈ Xw)
-#
-# Yw_true = cat([reshape(Xw[:, j, :] * coefficients[j], (n, 1, my))
-#     for j in 1:nlevels+1]..., dims=2)
-# Y_true = imodwt_matrix(Yw_true, wt)
-# Yw1 = modwt_matrix(Y_true, wt, nlevels)
-# mean(abs2.(Yw1 .- Yw_true))
-#
-#
-# fit = wavelm(X, Y_true, wt, nlevels)
-# predict(fit)
-#
-#
-# plot(Y_true, color=:black, layout=(3,1), legend=false)
-# plot!(predict(fit), color=:red, layout=(3,1))
-#
-# BB = [Xw[:, j, :] \ Yw1[:, j, :] for j in 1:(nlevels+1)]
-# Yw_pred = cat([reshape(Xw[:, j, :] * BB[j], (n, 1, my))
-#     for j in 1:nlevels+1]..., dims=2)
-# Y_pred = imodwt_matrix(Yw_pred, wt)
-# plot!(Y_pred, color=:blue, layout=(3,1))
